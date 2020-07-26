2466345b9492cf937b79ae2eb2fd07b99e109bc5
George Amvrosiadis <gamvrosi@gmail.com>
2020-07-25 20:09:55 -0400

    Change graph labels

--- speedtest.sh
+++ speedtest.sh
@@ -3,14 +3,14 @@
 case $1 in
     config)
     echo "graph_category network"
-    echo "graph_title Speedtest"
+    echo "graph_title Internet Connection Speed"
     echo "graph_args --base 1000 -l 0"
-    echo "graph_vlabel DL / UL"
+    echo "graph_vlabel Download/Upload Speed (Mbps)"
     echo "graph_scale no"
-    echo "down.label DL"
+    echo "down.label Download"
     echo "down.type GAUGE"
     echo "down.draw LINE1"
-    echo "up.label UL"
+    echo "up.label Upload"
     echo "up.type GAUGE"
     echo "up.draw LINE1"
     echo "graph_info Graph of Internet Connection Speed"
