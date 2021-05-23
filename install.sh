8d261bbcd87927a4daadf41580e4278a2756dd10
root <jon@directfreight.com>
2021-05-23 11:50:29 -0500

    tweaks to install

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-export host="$1"
+export host="$1" 
+export args="$2" #for options like -issh.key
 if test -z "$host"; then
         export host="127.0.0.1"
 fi
-ssh root@$host apt-get install python-pip
-ssh root@$host pip install speedtest-cli
-ssh root@$host rm -f /usr/share/munin/plugins/speedtest /etc/munin/plugins/speedtest
-ssh root@$host rm -f /usr/share/munin/plugins/speedtest.sh /etc/munin/plugins/speedtest.sh
-scp speedtest.sh /usr/share/munin/plugins/speedtest
-ssh root@$host chmod a+x /usr/share/munin/plugins/speedtest
-ssh root@$host ln -s /usr/share/munin/plugins/speedtest /etc/munin/plugins/speedtest
+ssh $args root@$host "apt-get install python3-pip||apt-get install python-pip"
+ssh $args root@$host pip install speedtest-cli
+ssh $args root@$host rm -f /usr/share/munin/plugins/speedtest /etc/munin/plugins/speedtest
+ssh $args root@$host rm -f /usr/share/munin/plugins/speedtest.sh /etc/munin/plugins/speedtest.sh
+scp $args speedtest.sh root@$host:/usr/share/munin/plugins/speedtest
+ssh $args root@$host chmod a+x /usr/share/munin/plugins/speedtest
+ssh $args root@$host ln -s /usr/share/munin/plugins/speedtest /etc/munin/plugins/speedtest
 LINE='18,38,58 * * * * root (/usr/local/bin/speedtest-cli/speedtest-cli --simple||speedtest-cli --simple) > /var/log/munin/speedtest.out.new && mv -f /var/log/munin/speedtest.out.new /var/log/munin/speedtest.out'
-ssh root@$host "cat /etc/crontab |grep -v "speedtest-cli" >/tmp/crontab.tmp; cat /tmp/crontab.tmp >/etc/crontab; echo '$LINE' >>/etc/crontab"
-ssh root@$host service munin-node restart
+ssh $args root@$host "cat /etc/crontab |grep -v "speedtest-cli" >/tmp/crontab.tmp; cat /tmp/crontab.tmp >/etc/crontab; echo '$LINE' >>/etc/crontab"
+ssh $args root@$host service munin-node restart
