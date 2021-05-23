5b01b58f5b2fbdc0e017942ec2c8ad8ac809b6c4
root <jon@directfreight.com>
2021-05-23 10:57:14 -0500

    fix for cron add

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ ssh root@$host pip install speedtest-cli
 scp speedtest.sh /usr/share/munin/plugins/
 ssh root@$host chmod a+x /usr/share/munin/plugins/speedtest.sh
 ssh root@$host ln -s /usr/share/munin/plugins/speedtest.sh /etc/munin/plugins/speedtest.sh
-ssh root@$host service munin-node restart
 LINE='18,38,58 * * * * sudo (/usr/local/bin/speedtest-cli/speedtest-cli --simple||speedtest-cli --simple) > /var/log/munin/speedtest.out'
-ssh root@$host 'cat /etc/crontab |grep -v "speedtest-cli" >/tmp/crontab.tmp; cat /tmp/crontab.tmp >/etc/crontab; echo "$LINE" >>/etc/crontab'
+ssh root@$host "cat /etc/crontab |grep -v "speedtest-cli" >/tmp/crontab.tmp; cat /tmp/crontab.tmp >/etc/crontab; echo '$LINE' >>/etc/crontab"
+ssh root@$host service munin-node restart
