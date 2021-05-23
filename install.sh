7bd348703728e570283c0057b0e590970ba698c4
root <jon@directfreight.com>
2021-05-23 11:02:13 -0500

    temp file for log

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ ssh root@$host pip install speedtest-cli
 scp speedtest.sh /usr/share/munin/plugins/
 ssh root@$host chmod a+x /usr/share/munin/plugins/speedtest.sh
 ssh root@$host ln -s /usr/share/munin/plugins/speedtest.sh /etc/munin/plugins/speedtest.sh
-LINE='18,38,58 * * * * sudo (/usr/local/bin/speedtest-cli/speedtest-cli --simple||speedtest-cli --simple) > /var/log/munin/speedtest.out'
+LINE='18,38,58 * * * * root (/usr/local/bin/speedtest-cli/speedtest-cli --simple||speedtest-cli --simple) > /var/log/munin/speedtest.out.new && mv -f /var/log/munin/speedtest.out.new /var/log/munin/speedtest.out'
 ssh root@$host "cat /etc/crontab |grep -v "speedtest-cli" >/tmp/crontab.tmp; cat /tmp/crontab.tmp >/etc/crontab; echo '$LINE' >>/etc/crontab"
 ssh root@$host service munin-node restart
